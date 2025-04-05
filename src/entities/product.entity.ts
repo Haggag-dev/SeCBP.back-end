0ae54a3cc2d2a78d8a88d7674d18c182c8bf28b5
Ahmed-878 <ahmedabouelhaggago6@gmail.com>
2025-04-05 23:23:16 +0200

    Add a real image placeholder URL

--- src/entities/product.entity.ts
+++ src/entities/product.entity.ts
@@ -16,7 +16,7 @@ export class Product {
   @Column({
     type: 'varchar',
     length: '255',
-    default: 'https://placeholder.com/placeholder.jpg',
+    default: 'https://images.unsplash.com/photo-1649182785115-eb5bd1c4cd45?q=80&w=2127&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   })
   imageUrl: string;
 

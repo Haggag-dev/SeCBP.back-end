63e1ffb91d22ab97d059b6f3600c7ead81643221
Ahmed-878 <ahmedabouelhaggago6@gmail.com>
2025-04-13 21:53:41 +0200

    Add tests for finding and updating stock

--- src/products.service.spec.ts
+++ src/products.service.spec.ts
@@ -5,12 +5,13 @@ import { Product } from './entities/product.entity';
 import { EntityManager } from 'typeorm';
 import mockProductsRepository from './__test__mocks/productsRepository.mock';
 import mockProductsData from './__test__mocks/productsData.mock';
-import { NotFoundException } from '@nestjs/common';
+import { ConflictException, NotFoundException } from '@nestjs/common';
+import mockEntityManager from './__test__mocks/entityManager.mock';
+import { UpdateStockDto } from './dto/update-stock.dto';
 
 describe('ProductsService', () => {
   let productsService: ProductsService;
 
-  const mockEntityManager = {};
   const correctProductId = 1;
   const falseProductId = -1;
 
@@ -64,6 +65,50 @@ describe('ProductsService', () => {
         expect(productsService.findOne(falseProductId)).rejects.toThrow(
           `Product with id ${falseProductId}`,
         );
+      });
+    });
+  });
+
+  describe('findStockLevel', () => {
+    it('should return the correct stock level of the given product id', () => {
+      expect(productsService.findStockLevel(correctProductId)).resolves.toEqual(
+        mockProductsData[correctProductId].stock,
+      );
+    });
+  });
+
+  describe('updateStockLevel', () => {
+    const sufficientUpdateAmount: UpdateStockDto = { amount: 10 };
+    const insufficientUpdateAmount: UpdateStockDto = { amount: 500 };
+
+    describe('given that there is sufficient stock amount', () => {
+      it('should return undefined', () => {
+        expect(
+          productsService.updateStockLevel(
+            correctProductId,
+            sufficientUpdateAmount,
+          ),
+        ).resolves.toBe(undefined);
+      });
+    });
+
+    describe('given that there is insufficient stock amount', () => {
+      it('should throw a ConflictException', () => {
+        expect(
+          productsService.updateStockLevel(
+            correctProductId,
+            insufficientUpdateAmount,
+          ),
+        ).rejects.toThrow(ConflictException);
+      });
+
+      it('should throw a ConflictException with the correct message', () => {
+        expect(
+          productsService.updateStockLevel(
+            correctProductId,
+            insufficientUpdateAmount,
+          ),
+        ).rejects.toThrow('There is not enough stock for this product.');
       });
     });
   });

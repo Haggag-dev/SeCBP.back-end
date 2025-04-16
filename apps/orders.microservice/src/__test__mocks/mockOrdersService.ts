fc838217f05795ed7fc4a99e69af3242466d2a3d
Ahmed-878 <ahmedabouelhaggago6@gmail.com>
2025-04-16 15:30:02 +0200

    Add Orders service mock

--- apps/orders.microservice/src/__test__mocks/mockOrdersService.ts
+++ apps/orders.microservice/src/__test__mocks/mockOrdersService.ts
@@ -1,3 +1,32 @@
-const mockOrdersService = {};
+import { NotFoundException } from '@nestjs/common';
+import { Order } from '../entities/orders.entity';
+import mockOrdersRepository from './mockOrdersRepository';
+import { CreateOrderDto } from '../dto/create-order.dto';
+
+const mockOrdersService = {
+  findAll: jest.fn().mockImplementation(async (user_id: number) => {
+    return mockOrdersRepository.findBy({ user_id });
+  }),
+
+  findOne: jest.fn().mockImplementation(async (order_id: number) => {
+    const order: Order | null = await mockOrdersRepository.findOneBy({
+      order_id,
+    });
+
+    if (!order)
+      throw new NotFoundException(`Order with id ${order_id} does not exist`);
+
+    return order;
+  }),
+
+  create: jest
+    .fn()
+    .mockImplementation(
+      async (createOrderDto: CreateOrderDto): Promise<Order> => {
+        const order = mockOrdersRepository.create(createOrderDto);
+        return mockOrdersRepository.save(order);
+      },
+    ),
+};
 
 export default mockOrdersService;

2519388e5b7efdfc07a73fce7e3570f8b7e794dc
Ahmed-878 <ahmedabouelhaggago6@gmail.com>
2025-04-16 17:08:52 +0200

    Add controller add its corresponding tests

--- apps/orders.microservice/src/orders.controller.spec.ts
+++ apps/orders.microservice/src/orders.controller.spec.ts
@@ -2,6 +2,10 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 import mockOrdersService from './__test__mocks/mockOrdersService';
+import { Order } from './entities/orders.entity';
+import mockOrdersData from './__test__mocks/mockOrdersData';
+import { NotFoundException } from '@nestjs/common';
+import mockCreateOrderDto from './__test__mocks/mockCreateOrderDto';
 
 describe('OrdersController', () => {
   let ordersController: OrdersController;
@@ -20,5 +24,74 @@ describe('OrdersController', () => {
 
   it('should be defined', () => {
     expect(ordersController).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    const user_id = '1';
+
+    it('should return an array of orders', async () => {
+      const result = await ordersController.findAll(user_id);
+      expect(Array.isArray(result)).toBe(true);
+      expect(result[0]).toBeInstanceOf(Order);
+
+      expect(mockOrdersService.findAll).toHaveBeenCalledWith(+user_id);
+    });
+
+    it('should return orders with the given user_id', async () => {
+      await expect(ordersController.findAll(user_id)).resolves.toEqual([
+        mockOrdersData[0],
+        mockOrdersData[1],
+      ]);
+
+      expect(mockOrdersService.findAll).toHaveBeenCalledWith(+user_id);
+    });
+  });
+
+  describe('findOne', () => {
+    describe('given an id of an order that exists', () => {
+      const order_id = '1';
+
+      it('should return an order object', async () => {
+        expect(ordersController.findOne(order_id)).resolves.toEqual(
+          mockOrdersData[0],
+        );
+
+        expect(mockOrdersService.findOne).toHaveBeenCalledWith(+order_id);
+      });
+    });
+
+    describe("given an id of an order that doesn't exist", () => {
+      const order_id = '-1';
+
+      it('should throw a NotFoundException', async () => {
+        expect(ordersController.findOne(order_id)).rejects.toThrow(
+          NotFoundException,
+        );
+
+        expect(mockOrdersService.findOne).toHaveBeenCalledWith(+order_id);
+      });
+
+      it('should throw a NotFoundException with the correct message', () => {
+        expect(ordersController.findOne(order_id)).rejects.toThrow(
+          `Order with id ${order_id} does not exist`,
+        );
+
+        expect(mockOrdersService.findOne).toHaveBeenCalledWith(+order_id);
+      });
+    });
+  });
+
+  describe('create', () => {
+    describe('given a correct CreateOrderDto', () => {
+      it('should return an Order object', () => {
+        expect(
+          ordersController.create(mockCreateOrderDto),
+        ).resolves.toBeInstanceOf(Order);
+
+        expect(mockOrdersService.create).toHaveBeenCalledWith(
+          mockCreateOrderDto,
+        );
+      });
+    });
   });
 });
--- apps/orders.microservice/src/orders.controller.ts
+++ apps/orders.microservice/src/orders.controller.ts
@@ -1,13 +1,25 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { Order } from './entities/orders.entity';
+import { CreateOrderDto } from './dto/create-order.dto';
 
 @Controller()
 export class OrdersController {
-  constructor(private readonly ordersMicroserviceService: OrdersService) {}
+  constructor(private readonly ordersService: OrdersService) {}
 
-  @Get('/orders/:user_id')
+  @Get('/users/:user_id/orders')
   findAll(@Param('user_id') user_id: string): Promise<Order[]> {
-    return this.ordersMicroserviceService.findAll(+user_id);
+    return this.ordersService.findAll(+user_id);
+  }
+
+  @Get('/orders/:order_id')
+  findOne(@Param('order_id') order_id: string): Promise<Order> {
+    return this.ordersService.findOne(+order_id);
+  }
+
+  @Post('/order')
+  @HttpCode(202)
+  create(@Body() createOrderDto: CreateOrderDto): Promise<Order> {
+    return this.ordersService.create(createOrderDto);
   }
 }

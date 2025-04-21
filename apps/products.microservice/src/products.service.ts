fb1575f386f3f0f1835837b61b28dede28ddc484
Ahmed-878 <ahmedabouelhaggago6@gmail.com>
2025-04-21 16:33:43 +0200

    Add RMQ client dependency

--- apps/products.microservice/src/products.service.ts
+++ apps/products.microservice/src/products.service.ts
@@ -1,5 +1,6 @@
 import {
   ConflictException,
+  Inject,
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
@@ -7,10 +8,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { EntityManager, Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
 import { UpdateStockDto } from './dto/update-stock.dto';
+import { ClientProxy, EventPattern, Payload } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class ProductsService {
   constructor(
+    @Inject('CONFIRM_STOCK_RESERVATION')
+    private client: ClientProxy,
     @InjectRepository(Product)
     private readonly productsRepository: Repository<Product>,
     private readonly entityManager: EntityManager,
@@ -46,5 +51,35 @@ export class ProductsService {
 
     product.stock -= updateAmount;
     await this.entityManager.save(product);
+  }
+
+  @EventPattern('reserve_stock')
+  async handleStockReservation(
+    @Payload()
+    order: {
+      order_id: number;
+      user_id: number;
+      product_id: number;
+      stock: number;
+    },
+  ) {
+    try {
+      await this.updateStockLevel(order.product_id, { amount: order.stock });
+      await firstValueFrom(
+        this.client.emit('stock_reserved', {
+          ...order,
+          status: 'confirmed',
+          message: 'success',
+        }),
+      );
+    } catch (err) {
+      await firstValueFrom(
+        this.client.emit('stock_reserved', {
+          ...order,
+          status: 'rejected',
+          message: err.message,
+        }),
+      );
+    }
   }
 }
